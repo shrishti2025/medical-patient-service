b24202cc35fa6903786a711ffd3e007b15005222
shrishti2025 <jainshrishti1002@gmail.com>
2026-01-20 12:22:45 +0530

    Initial Spring Boot setup for medical-patient-service

---
+++ src/main/java/com/hospital/medical_patient_service/MedicalPatientServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hospital.medical_patient_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalPatientServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalPatientServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hospital/medical_patient_service/MedicalPatientServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hospital.medical_patient_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalPatientServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
